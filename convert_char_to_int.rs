488cb11807f83360d7b9e6b704cb29cd62edbe8d
Dadmehr <134191240+BDadmehr0@users.noreply.github.com>
2023-09-12 14:48:23 +0330

    Delete convert_char_to_int.rs

--- convert_char_to_int.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    let character: char = 'B';
-
-    // convert char type to u8 integer type
-    let integer = character as u8;
-
-    println!("character = {}", character);
-    println!("integer = {}", integer);
-}
